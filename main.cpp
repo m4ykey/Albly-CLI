e946c247d780fd2a9fa1dffe50a53f877862beea
Michał <michalfr1601@gmail.com>
2026-07-21 18:40:10 +0200

    Add home screen

--- main.cpp
+++ main.cpp
@@ -5,14 +5,29 @@ using namespace ftxui;
 int main() {
     auto screen = ScreenInteractive::TerminalOutput();
 
-    auto button = Button(
-        "Exit",
-        [&] {
-            screen.Exit();
-        }
-    );
+    std::vector<std::string> entries = {
+        "Collection",
+        "Search",
+        "New Release",
+        "Listen Later",
+        "Settings"
+    };
 
-    screen.Loop(button);
+    int selected = 0;
+
+    auto menu = Menu(&entries, &selected);
+
+    auto renderer = Renderer(menu, [&] {
+        return vbox({
+            text("Albly") | bold | center,
+            separator(),
+            menu->Render(),
+            separator(),
+            text("↑ ↓ Choose | Enter Select")
+        }) | border;
+    });
+
+    screen.Loop(renderer);
 
     return 0;
 }
